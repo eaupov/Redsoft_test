8543dc11eaf816818f5398defbc7ff66e59a804e
eaupov <eaupov@gmail.com>
2015-08-06 00:29:33 +0500

    Верстка

---
+++ js/script.js
@@ -0,0 +1,57 @@
+(function($) {
+    $(function() {
+
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '"></a>';
+                }
+            });
+
+    });
+})(jQuery);
+
+
+$(function(){
+	$(".main-slider .jcarousel li").css({"width":$(".main-slider").css("width")});
+	
+	$('.read-more-320').click(function(){
+		$(".read-more-320-block").slideDown(1000);
+	});	
+	
+});
+
